c802589be9aa54d56a60c5369a21ad50a51a74b6
Diego17 <diegoalbertocastropastor@gmail.com>
2025-09-12 17:28:35 -0500

    feat: implement Express server with CORS and Helmet middleware

---
+++ backend/src/index.ts
@@ -0,0 +1,25 @@
+import express from "express"
+import cors from "cors"
+import helmet from "helmet"
+import "dotenv/config"
+import http from "http"
+
+const app = express()
+const server = http.createServer(app)
+
+app.use(express.json())
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:4321',
+    methods: ['GET', 'POST'],
+    allowedHeaders: "*",
+}))
+app.use(helmet())
+
+const PORT = process.env.PORT || 3000
+
+app.get("/", (_, res) => {
+    res.send("API is running...")
+})
+server.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`)
+})
